1f2bbbc01ca12bf4dccf9e9350dbacf71d160cc7
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2023-08-07 15:17:23 +0300

    Updated pubspec.yaml and added dice image with text button

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:academind_app/widgets/styledtext.dart';
+//import 'package:academind_app/widgets/styledtext.dart';
+
+rollDice() {}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,13 +10,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent,
+          backgroundColor: Colors.deepPurple,
         ),
-        body: const GradientContainer(
-          Colors.deepPurple, Colors.purpleAccent
-          )
-  
-        );
+        body: const GradientContainer(Colors.deepPurple, Colors.purpleAccent));
   }
 }
 
@@ -28,10 +26,32 @@ class GradientContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color1, color2], begin: Alignment.topLeft, end: Alignment.bottomRight)
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding:  const EdgeInsets.all(20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28
+                )
+              ),
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
       ),
-      child: const Center (child: StyledText('Hello World'),), 
     );
   }
 }
