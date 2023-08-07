35840bdbf40f2504ad06bab49e2a97bc55dde8cb
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2023-08-07 12:32:45 +0300

    Created the styledtext widget and GradientContainer class

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,24 +6,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      color: Colors.amber
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ));
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: const GradientContainer(Colors.deepPurple, Colors.purpleAccent),
+  
+        );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2], begin: Alignment.topLeft, end: Alignment.bottomRight)
+      ), 
+    );
   }
 }
